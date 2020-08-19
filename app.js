f343527885e1d7c9a5f63a3774e56618dfec9bdd
mizzerx <vuhunglcute@gmail.com>
2020-08-19 14:47:21 +0700

    Change to Server side validate

--- app.js
+++ app.js
@@ -41,6 +41,14 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
     var usr = req.body.usr;
     var pwd = req.body.pwd;
+    if (usr == "") {
+        var errMsg = "Must input username";
+        return res.render("Login.hbs", {e: errMsg});
+    }
+    if (pwd == "") {
+        var errMsg = "Must input password";
+        return res.render("Login.hbs", {e: errMsg});
+    }
     MongoConnect("toys")
         .then(db => {
             db.collection("login").find().toArray()
@@ -78,6 +86,32 @@ app.post("/addtoys", (req, res) => {
     var price = req.body.price;
     var status = req.body.status;
     var img = req.body.img;
+
+    if (name == "") {
+        var errMsg = "Must input name";
+        return res.render("AddToys.hbs", {e: errMsg});
+    }
+
+    if (price != "") {
+        if (isNaN(price)) {
+            var errMsg = "Just input price number only";
+            return res.render("AddToys.hbs", {e: errMsg});
+        }
+    } else {
+        var errMsg = "Must input price";
+        return res.render("AddToys.hbs", {e: errMsg});
+    }
+
+    if (img != "") {
+        regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (!regexp.test(img)) {
+            var errMsg = "Must input url type (https:// or http://)";
+            return res.render("AddToys.hbs", {e: errMsg});
+        }
+    } else {
+        var errMsg = "Must input image url";
+        return res.render("AddToys.hbs", {e: errMsg});   
+    }
     
     MongoConnect("toys")
     .then(db => {
